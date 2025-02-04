49e36d1fa78a1ba0ff0d58e751459ac3803dcac8
Ash DeClerk <ash.declerk@gmail.com>
2025-02-04 11:36:49 -0600

    Add ship and movement

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size = (SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        ship.update(dt)
         screen.fill((0, 0, 0))
+        ship.draw(screen)
         pygame.display.flip()
-        dt += clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
