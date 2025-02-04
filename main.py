8f08002af6ef8e4447f62bfc472b6ea516112389
Ash DeClerk <ash.declerk@gmail.com>
2025-02-04 11:50:11 -0600

    Add asteroids

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size = (SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        ship.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
         screen.fill((0, 0, 0))
-        ship.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
